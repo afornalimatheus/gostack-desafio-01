ea2a46073c8c272484cc4b5b9339c806f306ec80
Matheus <matheus.afornali@gobrax.com>
2020-02-04 08:51:14 -0300

    - Inserção de Middlewares. Signed-off-by: Matheus <afornalimatheus@gmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ const server = express();
 server.use(express.json());
 
 const projects = [];
+let numReqs  = 0;
+
+// Middleware GLOBAL
+server.use((req, res, next) => {
+  numReqs++;
+  console.time('Request');
+  console.log(`Método: ${req.method}; URL: ${req.url}; Número de requisições até o momento: ${numReqs}`);
+
+  next();
+
+  console.timeEnd('Request');
+});
+
+// Middlewares LOCAIS
+function checkIdInArray(req, res, next) {
+  if(!projects[req.params.id]) {
+    return res.status(400).json({error: 'Project dos not exists!'});
+  }
+
+  next();
+}
+
+function checkFieldExists(req, res, next) {
+  if(!req.body.title) {
+    return res.status(400).json({error: 'Param not found!'});
+  }
+
+  next();
+}
 
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
@@ -24,7 +53,7 @@ server.get('/projects', (req, res) => {
   res.json(projects);
 });
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', checkFieldExists, checkIdInArray, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -35,7 +64,7 @@ server.put('/projects/:id', (req, res) => {
   res.json(projects);
 });
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', checkFieldExists, checkIdInArray, (req, res) => {
   const { id } = req.params;
 
   projects.splice(id, 1);
@@ -43,7 +72,7 @@ server.delete('/projects/:id', (req, res) => {
   res.json(projects);
 });
 
-server.post('/projects/:id/tasks', (req, res) => {
+server.post('/projects/:id/tasks', checkFieldExists, checkIdInArray, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
